4583dd58c0bad0ac21caa00ed6ddd73bdf41fe2a
Izan <rrizanb@gmail.com>
2026-05-20 11:36:46 +0200

    Arreglando estructura de archivos

--- proyectoNTT/src/main/java/NTT/proyectoNTT/controller/TareaController.java
+++
@@ -1,5 +0,0 @@
-package NTT.proyectoNTT.controller;
-
-public class TareaController {
-
-}
--- proyectoNTT/src/main/java/NTT/proyectoNTT/controller/TareaRepository.java
+++
@@ -1,9 +0,0 @@
-package NTT.proyectoNTT.controller;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import NTT.proyectoNTT.entity.Tarea;
-
-public interface TareaRepository extends JpaRepository<Tarea, Long>{
-
-}
