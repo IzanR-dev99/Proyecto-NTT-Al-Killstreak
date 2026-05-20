2451b9015b3f994c048d46552ed5101f73a96d35
isrra29 <114836481+isrra29@users.noreply.github.com>
2026-05-20 10:48:39 +0200

    Crear entidad Tarea y Crear repositorio

---
+++ proyectoNTT/src/main/java/NTT/proyectoNTT/controller/TareaRepository.java
@@ -0,0 +1,9 @@
+package NTT.proyectoNTT.controller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import NTT.proyectoNTT.entity.Tarea;
+
+public interface TareaRepository extends JpaRepository<Tarea, Long>{
+
+}
---
+++ proyectoNTT/src/main/java/NTT/proyectoNTT/entity/Tarea.java
@@ -0,0 +1,59 @@
+package NTT.proyectoNTT.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Tarea {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String descripcion;
+    private boolean completada;
+
+    public Tarea() {
+    }
+
+    public Tarea(String titulo, String descripcion, boolean completada) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.completada = completada;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+}
